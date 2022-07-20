d285d0fab2a94d0a4d68ab93e0134123bf58120a
unknown <grabbany9258>
2022-07-20 19:06:29 +0600

    20

---
+++ php/20july2022/basic.php
@@ -0,0 +1,29 @@
+<?php
+
+    echo "Hello World Again!";
+
+    //Variable
+
+    $abc = "Today is Wednesday";
+    echo "<br>";
+    echo $abc;
+
+    $number =100;
+    $text ="Some text";
+
+    echo $number,$text;
+
+    
+    echo "<br>";
+    $fruits = array("Apple","Orange","Banana","Grape");
+    var_dump($fruits);
+
+    echo "<br>";
+    print_r($fruits);
+
+    echo "<br>";
+    echo "<pre>";
+
+    print_r($_SERVER);
+
+?>

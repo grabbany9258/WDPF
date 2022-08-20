8e54c557d308df4d52d1955bf6a390cf01c5e9ad
unknown <grabbany9258>
2022-08-20 18:34:14 +0600

    home

---
+++ mysql/20August2022/dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+//Mysql connection and Database selection
+   $db=  new mysqli("localhost", "root", "", "wdpf51_sampledb");
+
+//    if($db){
+//     print_r($db);
+//     echo "Success";
+//    }
+
+?>

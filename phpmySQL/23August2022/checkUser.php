d542cb77e477be1f45bbc46edba2208b91af6c07
unknown <grabbany9258>
2022-08-23 18:41:51 +0600

    class

---
+++ phpmySQL/23August2022/checkUser.php
@@ -0,0 +1,32 @@
+<?php
+include_once('dbconfig.php');
+
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+  echo "<br>";
+
+   $pass = sha1($pass);
+
+  //echo "SELECT * FROM users WHERE email = '$email' AND password = '$pass'";
+
+   $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$pass'";
+   $result = $db->query($sql);
+   if ($result->num_rows!=1){
+        $msg = "Email or password may be wrong";
+        header("Location:index.php?m=$msg");
+   }
+   else{
+    session_start();
+    $_SESSION['email'] = $email;
+    header("Location:dashboard.php");
+}
+
+       //echo "Email or password may be wrong";
+    
+    // include_once('index.php');
+//     header("Location:index.php?m=$msg"); // redirect kore fire asbe same page a.. 
+//    }
+    
+
+
+?>
---
+++ phpmySQL/23August2022/index.php
@@ -0,0 +1,20 @@
+<h1>Login Form</h1>
+<form action="checkUser.php" method="post">
+    Email: <br>
+    <input type="text" name="email" placeholder="Enter your Email"> <br>
+    Password: <br>
+    <input type="password" name="pass" placeholder="Enter your Password"> <br> <br>
+    <input type="submit" name="submit" value="LOGIN"> 
+
+</form>
+
+<?php
+if(isset($_GET['m'])){
+    $msg =  $_GET['m'];
+    echo  $msg;
+}
+
+   
+
+
+?>

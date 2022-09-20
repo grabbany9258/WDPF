b6df2c95770ffad841db17d5115f3552100c9bc4
unknown <grabbany9258>
2022-09-20 19:09:17 +0600

    class

---
+++ jQuery/class_7_20Sep2022/list.php
@@ -0,0 +1,27 @@
+<?php
+// echo "Hello";
+ echo $_GET['mydata'];
+
+if($_GET['mydata']=='Good'){
+    $good = array("aminul", "sakib", "fayzullah","rahi");
+    echo getNames($good);
+}
+else if($_GET['mydata']=='Bad'){
+    $bad = array("sohel", "joynal","rakib", "mahir");
+    echo getNames($bad);
+}
+
+
+function getNames($items){
+    $output = "<ul>";
+    for($i=0; $i<count($items); $i++){
+        $output .= "<li>" .$items[$i] . "</li>";
+
+    }
+    $output .= "</ul>";
+    return $output;
+
+}
+
+// echo getNames($persons);
+?>

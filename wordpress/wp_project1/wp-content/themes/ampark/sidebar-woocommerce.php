dd87e46114ef98b1f26f79d23d1b87cc12211da5
g_rabbany <golamrabbany10@gmailcom>
2022-10-04 09:31:33 +0600

    home changes wordpress

--- wordpress/wp_project1/wp-content/themes/ampark/sidebar-woocommerce.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! is_active_sidebar( 'avril-woocommerce-sidebar' ) ) {	return; } ?>
-<div id="av-secondary-content" class="av-column-4 mb-6 mb-av-0 wow fadeInUp">
-	<section class="sidebar">
-		<?php dynamic_sidebar('avril-woocommerce-sidebar'); ?>
-	</section>
-</div>

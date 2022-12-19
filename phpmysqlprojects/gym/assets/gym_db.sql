cdc41918f72164b9ed9259184338c81aef3aa72e
g_rabbany <golamrabbany10@gmailcom>
2022-12-19 21:50:42 +0600

    mens selun project

---
+++ phpmysqlprojects/gym/assets/gym_db.sql
@@ -0,0 +1,332 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2022 at 08:32 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gym_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(30) NOT NULL,
+  `member_id` int(30) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`id`, `member_id`, `firstname`, `middlename`, `lastname`, `gender`, `contact`, `address`, `email`, `date_created`) VALUES
+(5, 58487246, 'Anamul ', 'islam', 'durjoy', 'Male', '01715647896', 'Polton Dhaka -1207', 'anamul@gmail.com', '2020-10-21 13:18:19'),
+(6, 59430244, 'Naimur', 'islam', 'Naim', 'Female', '0164587958', 'Bashabo Central Road ', 'naimur@gmail.com', '2020-10-21 14:57:54'),
+(7, 58487247, 'Nahid', 'jaman', 'saif', 'male', '0189658745', 'Kalyanpur Dhaka', 'nahid@gmail.com', '2022-09-18 09:34:13'),
+(8, 58487248, 'sakib', 'al', 'hasan', 'male', '01789654780', 'Kamrangichor dhaka', 'sakib@gmail.com', '2022-09-18 09:34:13'),
+(9, 58487245, 'Anim', 'sahriar', 'Turjo', 'Male', '0154896587', 'Dhanmondi Dhaka 1207', 'anim@gmail.com', '2022-09-18 09:36:53'),
+(10, 58487249, 'Rakibul', 'Sahik', 'tuhin', 'male', '0164587565', 'Kolabagan Dhaka 1207', 'rakib@gmail.com', '2022-09-18 09:36:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+CREATE TABLE `packages` (
+  `id` int(30) NOT NULL,
+  `package` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `amount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`id`, `package`, `description`, `amount`) VALUES
+(2, 'Arm Building', 'Arm building with shape ', 3500),
+(4, 'Kitto diet', 'With diet weight decrease + trainer fee', 6000),
+(5, 'Muscle building ', 'muscle will be nearly twice as normal shape right now.', 3000),
+(6, '70kg Fitness', 'Lite Healthy Fitness ', 5000),
+(7, 'Diet Control', 'Fat Body shape ', 8000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(30) NOT NULL,
+  `registration_id` int(30) NOT NULL,
+  `amount` int(30) NOT NULL,
+  `remarks` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=registration, 2= monthly payment',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `registration_id`, `amount`, `remarks`, `type`, `date_created`) VALUES
+(1, 2, 4500, 'First payment', 2, '2020-10-21 14:39:26'),
+(2, 2, 3500, 'payment for november', 2, '2020-10-21 14:39:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plans`
+--
+
+CREATE TABLE `plans` (
+  `id` int(30) NOT NULL,
+  `plan` int(30) NOT NULL,
+  `amount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `plans`
+--
+
+INSERT INTO `plans` (`id`, `plan`, `amount`) VALUES
+(4, 6, 5000),
+(5, 3, 3000),
+(6, 2, 2500),
+(7, 12, 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration_info`
+--
+
+CREATE TABLE `registration_info` (
+  `id` int(30) NOT NULL,
+  `member_id` int(30) NOT NULL,
+  `plan_id` int(30) NOT NULL,
+  `package_id` int(30) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `trainer_id` tinyint(30) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0=Inactive, 1= Active',
+  `date_created` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `registration_info`
+--
+
+INSERT INTO `registration_info` (`id`, `member_id`, `plan_id`, `package_id`, `start_date`, `end_date`, `trainer_id`, `status`, `date_created`) VALUES
+(2, 5, 1, 2, '2020-10-21', '2021-10-21', 0, 0, '2020-10-21'),
+(3, 5, 1, 2, '2020-10-21', '2021-10-21', 0, 1, '2020-10-21'),
+(4, 6, 1, 2, '2019-10-19', '2020-10-19', 0, 0, '2020-10-21'),
+(5, 6, 1, 2, '2020-10-21', '2021-10-21', 0, 1, '2020-10-21'),
+(6, 0, 4, 5, '2022-09-18', '2023-03-18', 1, 1, '2022-09-18'),
+(7, 7, 5, 5, '2022-09-18', '2022-12-18', 1, 1, '2022-09-18'),
+(8, 10, 5, 4, '2022-09-18', '2022-12-18', 3, 1, '2022-09-18'),
+(9, 8, 4, 6, '2022-09-18', '2023-03-18', 4, 1, '2022-09-18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int(30) NOT NULL,
+  `member_id` int(30) NOT NULL,
+  `dow` text NOT NULL,
+  `date_from` date NOT NULL,
+  `date_to` date NOT NULL,
+  `time_from` time NOT NULL,
+  `time_to` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `member_id`, `dow`, `date_from`, `date_to`, `time_from`, `time_to`) VALUES
+(1, 0, '1', '0000-00-00', '2022-12-31', '00:00:00', '00:00:00'),
+(2, 5, '1', '0000-00-00', '2022-12-31', '00:00:00', '00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trainers`
+--
+
+CREATE TABLE `trainers` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `rate` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `trainers`
+--
+
+INSERT INTO `trainers` (`id`, `name`, `contact`, `email`, `rate`) VALUES
+(1, 'Golam Rabbany', '01642541075', 'golamrabbany10@gmail.com', 500),
+(2, 'Sakib Hayder', '01716699548', 'sakib@gmail.com', 500),
+(3, 'Rahi jaman', '01896547896', 'rahi@gmail.com', 450),
+(4, 'Ruhul amin', '0178965489', 'ruhul@gmail.com', 400);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 3 COMMENT '1=Admin,2=Staff, 3= subscriber'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+(1, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `plans`
+--
+ALTER TABLE `plans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration_info`
+--
+ALTER TABLE `registration_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trainers`
+--
+ALTER TABLE `trainers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `packages`
+--
+ALTER TABLE `packages`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `plans`
+--
+ALTER TABLE `plans`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `registration_info`
+--
+ALTER TABLE `registration_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `trainers`
+--
+ALTER TABLE `trainers`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
